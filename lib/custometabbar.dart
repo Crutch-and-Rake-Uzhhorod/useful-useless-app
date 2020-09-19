9275b846368792da2194c589cfa4b0d88471f481
BruSD <brusd.com@gmail.com>
2020-09-19 17:08:02 +0300

    fix code convention

--- lib/custometabbar.dart
+++ lib/custometabbar.dart
@@ -9,20 +9,24 @@ class CustomeTabBar extends StatefulWidget {
 
 class _CustomeTabBarState extends State<CustomeTabBar> {
   final controller = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Demo Home Page"),
+        title: Text('Flutter Demo Home Page'),
         centerTitle: true,
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(kToolbarHeight),
           child: TabbarHeader(
             controller: controller,
             tabs: [
-              Tab(icon: Icon(Icons.map), text: "Map"),
-              Tab(icon: Icon(Icons.list), text: "List"),
-              Tab(icon: Icon(Icons.person), text: "User profile", ),
+              Tab(icon: Icon(Icons.map), text: 'Map'),
+              Tab(icon: Icon(Icons.list), text: 'List'),
+              Tab(
+                icon: Icon(Icons.person),
+                text: 'User profile',
+              ),
             ],
           ),
         ),
