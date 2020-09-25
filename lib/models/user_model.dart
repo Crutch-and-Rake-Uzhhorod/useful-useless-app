9cfee37e200719f3f5086b275b91b563737807c0
ultrasosok <mousej92@gmail.com>
2020-09-25 17:34:48 +0300

    fix
    fix user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -4,14 +4,18 @@ part 'user_model.g.dart';
 
 @JsonSerializable()
 
-class UserModel{
+class UserModel {
   String auth_type;
   bool notifycation_settings;
   List<String> user_houses;
 
+  // ignore: sort_constructors_first
   UserModel({this.auth_type, this.notifycation_settings, this.user_houses});
 
-  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+  // ignore: sort_constructors_first
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$UserModelToJson(this);
+
 }
