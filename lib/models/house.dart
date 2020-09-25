3e4c29f2699d00ddddff630a6147e9bdf386f75a
Dmytro Kosto <dmykos@gmail.com>
2020-09-25 18:59:29 +0300

    HouseModel: delete void main()

--- lib/models/house.dart
+++ lib/models/house.dart
@@ -4,20 +4,6 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'house.g.dart';
 
-void main() {
-
-  var  jsonString = '{ "street" : "Швабска", "building_number" : "51", "city" : "Uzhhorod", "location" : { 	"lang" : "0.0", 	"lat" : "0.0" 	}, "users" : [ "user_id_from_auth" ] }';
-
-  Map houseMap = jsonDecode(jsonString);
-  var house = House.fromJson(houseMap);
-  print(house.street);
-  print(house.building_number);
-  print(house.city);
-  print(house.location);
-  print(house.users);
-  print(house.toJson());
-}
-
 @JsonSerializable()
 class House {
   House({
