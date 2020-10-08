789d3e70141c2210c399d98448b7eac62af1706b
Dmytro Kosto <dmykos@gmail.com>
2020-10-08 11:22:36 +0300

    Map Tab 0: Provider Implamentation: class PowerOffProvider

--- lib/providers/power_off_provider.dart
+++ lib/providers/power_off_provider.dart
@@ -1,6 +1,34 @@
 import 'package:flutter/foundation.dart';
+import 'package:useful_useless_app/models/house.dart';
 
 class PowerOffProvider with ChangeNotifier {
 
+  final List<House> _locations = [
+    House(
+        street: 'Швабска',
+        building_number: '51',
+        city: 'Uzhhorod',
+        location: {
+          'lang' : '0.0',
+          'lat' : '0.0'
+        },
+        users: []
+    ),
+  ];
+
+  DateTime _date;
+
+  List<House> get locations {
+    return [..._locations];
+  }
+
+  DateTime get date {
+    return _date;
+  }
+
+  void setDate(DateTime date) {
+    _date = date;
+    notifyListeners();
+  }
 }
 
