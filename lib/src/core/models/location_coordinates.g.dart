8b0acf74b18a7bd63d777b9cd31b0dd76043a0d7
Ishchik <lyubomyr2030@gmail.com>
2021-08-24 03:03:49 +0300

    Add missing model

---
+++ lib/src/core/models/location_coordinates.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'location_coordinates.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LocationCoordinates _$LocationCoordinatesFromJson(Map<String, dynamic> json) {
+  return LocationCoordinates(
+    lat: (json['lat'] as num).toDouble(),
+    lng: (json['lng'] as num).toDouble(),
+  );
+}
