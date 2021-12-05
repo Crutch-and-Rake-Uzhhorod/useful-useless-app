2532e49d547af343b5c3742a8341c33006e10802
Ishchik <lyubomyr2030@gmail.com>
2021-12-05 22:43:05 +0200

    Fix warning

--- lib/src/web_ui/home_web/widgets/calendar_scroll_web_widget.dart
+++ lib/src/web_ui/home_web/widgets/calendar_scroll_web_widget.dart
@@ -1,7 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
-import 'package:easy_localization/easy_localization.dart';
 
 import '../../../core/provider/calendar_scroll_provider.dart';
 
